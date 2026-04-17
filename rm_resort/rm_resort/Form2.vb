c8a47481bac9c49f4b2b4e65d11b51a42d6053ee
yhanbel75-lab <gianestrella2007@gmail.com>
2026-04-17 23:32:22 +1200

    testing logo

--- rm_resort/rm_resort/Form2.vb
+++ rm_resort/rm_resort/Form2.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form2
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) 
 
     End Sub
 End Class
